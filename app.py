0679d44af3d47a0c652b56b350f8de0410d231b8
Ashwath <ashwathguru.s@bnedloudcloud.com>
2023-11-29 18:28:03 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import openai
+import streamlit as st
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+import speech_recognition as sr
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+def main():
+    st.header("Chat with TicketGPT")
+    st.title("Voice-to-Text App")
+
+    # Button to start/stop microphone input
+    button_start = st.button("Start Microphone Input")
+    button_stop = st.button("Stop Microphone Input", key="stop")
+
+    if button_start:
+        st.experimental_rerun()
+
+    if button_stop:
+        st.session_state.stop = True
+
+    if "stop" in st.session_state:
+        st.stop()
+
+    # Process voice input
+    text = transcribe_microphone()
+    st.text("Transcribed Text:")
+    st.text(text)
+    query = text
+    button = st.button("Submit")
+    if button:
+      st.write(get_answer_csv(query))
+
+def transcribe_microphone():
+    recognizer = sr.Recognizer()
+
+    # Capture audio from microphone with a 15-second timeout
+    with sr.Microphone() as source:
+        st.text("Listening... Speak something.")
+        try:
+            audio_data = recognizer.listen(source, timeout=15)
+        except sr.WaitTimeoutError:
+            return "Recording timed out after 15 seconds."
+
+    # Recognize speech using Google Web Speech API
+    try:
+        text = recognizer.recognize_google(audio_data)
+        return text
+    except sr.UnknownValueError:
+        return "Could not understand audio"
+    except sr.RequestError as e:
+        return f"Error connecting to Google Speech Recognition service: {e}"
+
+def get_answer_csv(query: str) -> str:
+    file = "raw.csv"
+    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=False)
+    answer = agent.run(query)
+    return answer
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
