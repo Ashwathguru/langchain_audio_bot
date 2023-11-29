5a2c5fd86c70c2d3c12e49bcef00dcd654c7acb1
Ashwath <ashwathguru.s@bnedloudcloud.com>
2023-11-29 19:00:10 +0530

    only voice

--- app.py
+++ app.py
@@ -1,72 +1,59 @@
-import openai
 import streamlit as st
+import sounddevice as sd
+import speech_recognition as sr
+import threading
+import time
+import openai
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
-import speech_recognition as sr
+
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def main():
-    st.header("Chat with TicketGPT")
-    st.title("Voice-to-Text App")
+def record_audio():
+    duration = 15  # seconds
+    fs = 44100  # sampling rate
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype='int16')
+    sd.wait()
 
-   # Button to start/stop microphone input
-    if "stop" not in st.session_state:
-        st.session_state.stop = False
+    return recording
 
-    button_start = st.button("Start Microphone Input")
-    button_stop = st.button("Stop Microphone Input")
-
-    if button_start:
-        st.session_state.stop = False
-        st.experimental_rerun()
-
-    if button_stop:
-        st.session_state.stop = True
-
-    if st.session_state.stop:
-        st.stop()
-
-    # Process voice input
-    text = transcribe_microphone()
-    st.text("Transcribed Text:")
-    st.text(text)
-    query = text
-    button = st.button("Submit")
-    if button:
-      st.write(get_answer_csv(query))
-
-def transcribe_microphone():
+def transcribe_audio(audio_data):
     recognizer = sr.Recognizer()
-
-    # Capture audio from microphone with a 15-second timeout
-    with sr.Microphone() as source:
-        st.text("Listening... Speak something.")
-        try:
-            audio_data = recognizer.listen(source, timeout=15)
-        except sr.WaitTimeoutError:
-            return "Recording timed out after 15 seconds."
-
-    # Recognize speech using Google Web Speech API
+    audio = sr.AudioData(audio_data, sample_rate=44100, sample_width=2)
+    
     try:
-        text = recognizer.recognize_google(audio_data)
+        text = recognizer.recognize_google(audio)
         return text
     except sr.UnknownValueError:
-        return "Could not understand audio"
+        return "Speech Recognition could not understand audio"
     except sr.RequestError as e:
-        return f"Error connecting to Google Speech Recognition service: {e}"
+        return f"Error with the speech recognition service: {e}"
 
-def get_answer_csv(query: str) -> str:
-    file = "raw.csv"
-    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=False)
-    answer = agent.run(query)
-    return answer
+def main():
+    st.title("Voice Recorder and Transcriber")
+
+    recording = None
+    start_recording = st.button("Start Recording")
+
+    if start_recording:
+        st.info("Recording... Click 'Stop Recording' within 15 seconds.")
+        recording = record_audio()
+
+    if recording is not None:
+        st.success("Recording complete!")
+
+        stop_recording = st.button("Stop Recording")
+
+        if stop_recording:
+            st.info("Processing... Please wait.")
+            text = transcribe_audio(recording.tobytes())
+
+            st.text_area("Transcribed Text", text, height=200)
+            
+            if st.button("Submit"):
+                st.balloons()
+                print("Transcribed Text:", text)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
